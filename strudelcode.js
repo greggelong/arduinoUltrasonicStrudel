34feba646e9dec7df9c4104979afb41068721919
Greg Kreisman <gkreisman_cep@qq.com>
2026-06-30 18:41:25 +0800

    Add strudelcode.js for gamepad-controlled sound

---
+++ strudelcode.js
@@ -0,0 +1,21 @@
+// Your Arduino is gamepad #0
+const gp = gamepad(0);
+
+// Map distance to pitch (100Hz to 2000Hz)
+// Close = high pitch, Far = low pitch
+let freq = gp.x1.range(100, 2000)
+
+setCpm(140 / 4) // 140 BPM
+
+// Drums
+$: s("bd ~ bd ~")
+   .stack(s("~ sd ~ sd"))
+   .stack(s("hh ~ hh ~"))
+   .gain(0.3)
+
+// Melody controlled by distance
+$: note("c4")
+   .sound("vibraphone_soft")
+   .freq(freq)
+   .gain(0.9)
+   .release(0.2)
